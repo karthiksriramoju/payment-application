5943453799c0f58e771010037a29ea0f5af220b1
Your Name <karthiksriramoju11@gmail.com>
2024-08-15 23:45:09 +0530

    added pages

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -2,14 +2,45 @@ import express from 'express';
 import db from "@repo/db/client"
 const app = express();
 
-app.post("/hdfcWebhook",(req,res)=>{
-  const paymentINformation = {
+app.post("/hdfcWebhook",async(req,res)=>{
+  const paymentInformation = {
     token : req.body.token,
     userId: req.body.user_identifier,
     amount: req.body.amount
   };
 
-db.balance.update(paymentINformation.userId)
+  try {
+    await db.$transaction([
+        db.balance.updateMany({
+            where: {
+                userId: Number(paymentInformation.userId)
+            },
+            data: {
+                amount: {
+                    // You can also get this from your DB
+                    increment: Number(paymentInformation.amount)
+                }
+            }
+        }),
+        db.onRampTransaction.updateMany({
+            where: {
+                token: paymentInformation.token
+            }, 
+            data: {
+                status: "Success",
+            }
+        })
+    ]);
+
+    res.json({
+        message: "Captured"
+    })
+} catch(e) {
+    console.error(e);
+    res.status(411).json({
+        message: "Error while processing webhook"
+    })
+}
 
 })
 
---
+++ apps/user-app/(dashboard)/transactions/page.tsx
@@ -0,0 +1,10 @@
+
+const page = () => {
+  return (
+    <div>
+      Transaction
+    </div>
+  )
+}
+
+export default page
