3faa1caa7fd28919bf515c0c5f31eb4ce1c1fc67
Your Name <karthiksriramoju11@gmail.com>
2024-08-17 22:26:06 +0530

    added sidebar,/ui components

--- apps/user-app/app/components/AppbarClient.tsx
+++ apps/user-app/app/components/AppbarClient.tsx
@@ -1,24 +1,18 @@
 "use client"
-import {signIn, signOut, useSession } from "next-auth/react"
-import { Appbar } from "@repo/ui/appbar"
-import { useRouter } from "next/navigation"
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Appbar } from "@repo/ui/appbar";
+import { useRouter } from "next/navigation";
 
-
-const AppbarClient = () => {
-  const router=useRouter()
-  const session = useSession()
+export function AppbarClient() {
+  const session = useSession();
+  const router = useRouter();
 
   return (
-    <div>
-      <Appbar onSignin={signIn} onSignout={
-        async () => {
-          await signOut
-          router.push('/api/auth/signin')
-        }} user={session.data?.user}
-      />
-    </div>
-  )
+   <div>
+      <Appbar onSignin={signIn} onSignout={async () => {
+        await signOut()
+        router.push("/api/auth/signin")
+      }} user={session.data?.user} />
+   </div>
+  );
 }
-
-export default AppbarClient
-
