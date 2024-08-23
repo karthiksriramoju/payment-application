f0928ba0bf3203a32ab4143fadfc7162d8432e71
Your Name <karthiksriramoju11@gmail.com>
2024-08-23 18:20:13 +0530

    added p2p

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -2,41 +2,39 @@ import express from "express";
 import db from "@repo/db/client";
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.post("/hdfcWebhook", async (req, res) => {
-    //TODO: Add zod validation here?
-    //TODO: HDFC bank should ideally send us a secret so we know this is sent by them
-    const paymentInformation: {
-        token: string;
-        userId: string;
-        amount: string
-    } = {
+    const paymentInformation = {
         token: req.body.token,
         userId: req.body.user_identifier,
         amount: req.body.amount
     };
-    console.log(req.body.user_identifier)
-    console.log(req.body.token)
-    console.log(req.body.amount)
+    console.log(req.body.user_identifier);
+    console.log(req.body.token);
+    console.log(req.body.amount);
 
     try {
         await db.$transaction([
-            db.balance.updateMany({
+            db.balance.upsert({
                 where: {
                     userId: Number(paymentInformation.userId)
                 },
-                data: {
+                update: {
                     amount: {
-                        // You can also get this from your DB
                         increment: Number(paymentInformation.amount)
                     }
+                },
+                create: {
+                    userId: Number(paymentInformation.userId),
+                    amount: Number(paymentInformation.amount),
+                    locked: 0
                 }
             }),
             db.onRampTransaction.updateMany({
                 where: {
                     token: paymentInformation.token
-                }, 
+                },
                 data: {
                     status: "Success",
                 }
@@ -45,14 +43,13 @@ app.post("/hdfcWebhook", async (req, res) => {
 
         res.json({
             message: "Captured"
-        })
-    } catch(e) {
+        });
+    } catch (e) {
         console.error(e);
         res.status(411).json({
             message: "Error while processing webhook"
-        })
+        });
     }
-
-})
+});
 
 app.listen(3003);
