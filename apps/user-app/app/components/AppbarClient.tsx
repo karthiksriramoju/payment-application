82f2bf189b9079577ea5888f8753fa439407c8ec
Your Name <karthiksriramoju11@gmail.com>
2024-08-26 23:50:53 +0530

    logout redirect

--- apps/user-app/app/components/AppbarClient.tsx
+++ apps/user-app/app/components/AppbarClient.tsx
@@ -9,7 +9,7 @@ export function AppbarClient() {
 
   return (
    <div>
-      <Appbar onSignin={signIn} onSignout={async () => signOut({ callbackUrl: "/" })} // Redirects to home page after sign out
+      <Appbar onSignin={signIn} onSignout={async () => signOut({ callbackUrl: "http://ec2-13-60-180-62.eu-north-1.compute.amazonaws.com" })} // Redirects to home page after sign out
         user={session.data?.user} />
    </div>
   );
