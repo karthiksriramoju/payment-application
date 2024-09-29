d6cbbb0cb1d71e2d9973244ca448c66cd1c8eedb
Your Name <karthiksriramoju11@gmail.com>
2024-09-29 22:37:50 +0530

    changes aws account

--- apps/user-app/app/api/auth/signup.ts
+++ apps/user-app/app/api/auth/signup.ts
@@ -7,6 +7,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return res.status(405).json({ message: 'Method not allowed' });
   }
 
+  
   const { phone, password } = req.body;
 
   try {
